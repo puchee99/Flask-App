6685fa97489744f40f924f5f218d59f1bf53f218
Arnau Puche <40550304+puchee99@users.noreply.github.com>
2022-05-13 03:27:50 +0200

    Update models.py

--- app/mod_auth/models.py
+++ app/mod_auth/models.py
@@ -1,10 +1,5 @@
-#from sqlite3 import dbapi2
-#import sys
-#sys.path.append("..")
-#from run import db
-from app import db#, db_mongo
 
-#from library.scraper import data_scrapper
+from app import db#, db_mongo
 
 class Users(db.Model):
     # See http://flask-sqlalchemy.pocoo.org/2.0/models/#simple-example
@@ -15,14 +10,6 @@ class Users(db.Model):
     def __init__(self,name,email):
         self.name = name
         self.email = email
-"""
-class Users_mongo(db_mongo.Document):
-    name = db_mongo.StringField()
-    email = db_mongo.StringField()
-    def to_json(self):
-        return {"name": self.name,
-                "email": self.email}
-"""
 
 
 def create_user(new_name, new_email):
@@ -39,29 +26,3 @@ def create_user(new_name, new_email):
     db.session.commit()
 
     return usr
-
-
-
-#if __name__ == "__main__":
-
-    # Run this file directly to create the database tables.
-    #data_scrapper()
-    #db.create_all()
-    #python models.py to create db
-"""
-
-
-def create_pdf(df, name ="Stock"):
-    day = str(datetime.now()).split()[0]
-    canvas = Canvas("pdf/"+name+day+".pdf")
-    canvas.drawString(72, 72, "Hello, World")
-
-    canvas.save()
-    return
-    if "-t" in opts: #train
-        data = ql.call2()
-        print(data.head())
-        print(data.tail())
-
-    ml.run()
-"""
