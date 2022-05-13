2492f6c15c64f103040dddcdf427fec9f170aff2
Arnau Puche <40550304+puchee99@users.noreply.github.com>
2022-05-13 03:27:19 +0200

    Update views.py

--- app/mod_auth/views.py
+++ app/mod_auth/views.py
@@ -1,19 +1,11 @@
 from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for
-#import sys
-#sys.path.append("..")
 
-# Import the database object from the main app module
 from app import db
 
 # Import module models (i.e. User)
 from app.mod_auth.models import Users, create_user#, Users_mongo
-#from run import app
 
-
-
-
-################mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
 
 mod_auth = Blueprint('auth', __name__, url_prefix='')
 
@@ -22,55 +14,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app.mod_auth.forms import LoginForm
 import json
 from flask import Flask, request, jsonify
-"""
-#START MONGO
-@mod_auth.route('/movies')
-def  get_movies():
-    movies = Users_mongo.objects()
-    return  jsonify(movies), 200
-
-
-@mod_auth.route('/', methods=['GET'])
-def query_records():
-    name = request.args.get('name')
-    user = Users_mongo.objects(name=name).first()
-    if not user:
-        return jsonify({'error': 'data not found'})
-    else:
-        return jsonify(user.to_json())
-
-@mod_auth.route('/', methods=['PUT'])
-def create_record():
-    record = json.loads(request.data)
-    user = Users_mongo(name=record['name'],
-                email=record['email'])
-    user.save()
-    return jsonify(user.to_json())
-
-@mod_auth.route('/', methods=['POST'])
-def update_record():
-    record = json.loads(request.data)
-    user = Users_mongo.objects(name=record['name']).first()
-    if not user:
-        return jsonify({'error': 'data not found'})
-    else:
-        user.update(email=record['email'])
-    return jsonify(user.to_json())
-
-@mod_auth.route('/', methods=['DELETE'])
-def delete_record():
-    record = json.loads(request.data)
-    user = Users_mongo.objects(name=record['name']).first()
-    if not user:
-        return jsonify({'error': 'data not found'})
-    else:
-        user.delete()
-    return jsonify(user.to_json())
-
-#END MONGO
-"""
-
-###########@mod_auth.route('/login', methods=['GET', 'POST'])
 
 @mod_auth.route('/', methods=['GET', 'POST'])
 def login():
@@ -161,38 +104,3 @@ def signin():
 
     return render_template("auth/signin.html", form=form)
     
-
-
-
-
-
-
-"""
-@app.route('/')
-def index():
-
-    desserts = Dessert.query.all()
-
-    return render_template('index.html', desserts=desserts)
-
-
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-
-    if request.method == 'GET':
-        return render_template('add.html')
-
-    # Because we 'returned' for a 'GET', if we get to this next bit, we must
-    # have received a POST
-
-    # Get the incoming data from the request.form dictionary.
-    # The values on the right, inside get(), correspond to the 'name'
-    # values in the HTML form that was submitted.
-
-    dessert_name = request.form.get('name_field')
-    dessert_price = request.form.get('price_field')
-    dessert_cals = request.form.get('cals_field')
-
-    dessert = create_dessert(dessert_name, dessert_price, dessert_cals)
-    return render_template('add.html', dessert=dessert)
-"""
