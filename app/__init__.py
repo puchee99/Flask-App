bb22de7a000e8b6a5a6078d10c0bcdec3ed66f51
Arnau Puche <40550304+puchee99@users.noreply.github.com>
2022-05-13 03:24:38 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,3 @@
-#COPIAT TOT, REVISAR
-# 
-# 
-# 
-# # Import flask and template operators
-
-
-
-
-#RUN and search--> http://[your droplet's IP]/auth/signin
 from flask import Flask, render_template
 
 # Import SQLAlchemy
@@ -40,10 +30,6 @@ from app.mod_auth.views import mod_auth as auth_module
 
 # Register blueprint(s)
 app.register_blueprint(auth_module)
-#app.register_blueprint(auth_module_2)
-
-# app.register_blueprint(xyz_module)
-# ..
 
 # Build the database:
 # This will create the database file using SQLAlchemy
